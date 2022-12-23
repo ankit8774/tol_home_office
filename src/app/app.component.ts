f0dcf60fe9aa83eacd89466fce8328da85f5f076
ankit <ankitsrivastavaallahabad@gmail.com>
2022-12-23 18:24:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tol_home';
+  title = 'Tol_home_office';
 
   constructor(){}
 
